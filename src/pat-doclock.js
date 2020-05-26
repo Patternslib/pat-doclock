011a4922d5f47376d1f9aba3264d7b2477e9a694
Johannes Raggam <thetetet@gmail.com>
2020-05-26 09:45:04 +0200

    Upgrade to es6

--- src/pat-doclock.js
+++ src/pat-doclock.js
@@ -1,113 +1,93 @@
-(function(root, factory) {
-    // We use AMD (Asynchronous Module Definition) or browser globals to create
-    // this module.
-    if (typeof define === "function" && define.amd) {
-        define([
-            "jquery",
-            "pat-base",
-            "pat-registry",
-            "pat-parser",
-            "pat-logger",
-        ], function() {
-            return factory.apply(this, arguments);
-        });
-    } else {
-        // If require.js is not available, you'll need to make sure that these
-        // global variables are available.
-        factory($, patterns.Base, patterns, patterns.Parser, patterns.logger); // jshint ignore:line
-    }
-}(this, function($, Base, registry, Parser, logger) {
-    "use strict";
+import $ from "jquery";
+import Base from "patternslib/src/core/base";
+import Parser from "patternslib/src/core/parser";
+import registry from "patternslib/src/core/registry";
 
-    var parser = new Parser("doclock");
-    parser.add_argument("url", "");
+const parser = new Parser("doclock");
+parser.add_argument("url", "");
 
-    var log = logger.getLogger("pat-display-time");
-    log.debug("pattern loaded");
 
-    return Base.extend({
-        name: "doclock",
-        trigger: ".pat-doclock",
-        _changed: false,
-        defaults: {
-            // events on which to check for changes
-            changingEvents: "change keyup paste",
-            // fields on which to check for changes
-            changingFields: "input,select,textarea,fileupload,[contenteditable=true]"
-        },
-        init: function() {
-            this.options = parser.parse(this.$el);
-            log.debug("pattern initialized");
-            this.set_listeners();
-        },
-        inject_response: function(data) {
-            var $data = $("<div>" + data + "</div>");
-            $("#global-statusmessage").html(
-                $data.find("#global-statusmessage").html()
-            );
-            registry.scan($("#global-statusmessage"));
-            $(".quick-functions #saving-badge, .quick-functions #save-button").replaceWith(
-                $data.find("#content-core").html()
-            );
-            registry.scan($(".quick-functions #saving-badge, .quick-functions #save-button"));
-        },
-        lock: function() {
-            var self = this;
-            if (self._changed) {
-                return;
-            }
-            if (!self.options.url) {
-                return;
-            }
-            self._changed = true;
-            $.ajax({
-                url: self.options.url,
-                data: {
-                    "lock": true
-                },
-                success: this.inject_response.bind(this)
-            });
-        },
-        unlock: function() {
-            var self = this;
-            if (!self._changed) {
-                return;
-            }
-            if (!self.options.url) {
-                return;
-            }
-            $.ajax({
-                url: self.options.url,
-                data: {
-                    "unlock": true
-                },
-                success: this.inject_response.bind(this)
-            });
-            self._changed = false;
-        },
-        set_listeners: function() {
-            var self = this;
-            if (!self.$el.is("form")) {
-                return;
-            }
-            // unlock when changing page
-            $(window).on(
-                "beforeunload",
-                self.unlock.bind(self)
-            );
-
-            // unlock when the form gets removed from the DOM
-            self.$el.bind("DOMNodeRemoved", function(e) {
-                if (e.target === self.$el[0]) {
-                    self.unlock.bind(self)();
-                }
-            });
-
-            // lock when elements are changed
-            $(self.defaults.changingFields, self.$el).on(
-                self.defaults.changingEvents,
-                self.lock.bind(self)
-            );
+export default Base.extend({
+    name: "doclock",
+    trigger: ".pat-doclock",
+    _changed: false,
+    defaults: {
+        // events on which to check for changes
+        changingEvents: "change keyup paste",
+        // fields on which to check for changes
+        changingFields: "input,select,textarea,fileupload,[contenteditable=true]"
+    },
+    init: function() {
+        this.options = parser.parse(this.$el);
+        this.set_listeners();
+    },
+    inject_response: function(data) {
+        var $data = $("<div>" + data + "</div>");
+        $("#global-statusmessage").html(
+            $data.find("#global-statusmessage").html()
+        );
+        registry.scan($("#global-statusmessage"));
+        $(".quick-functions #saving-badge, .quick-functions #save-button").replaceWith(
+            $data.find("#content-core").html()
+        );
+        registry.scan($(".quick-functions #saving-badge, .quick-functions #save-button"));
+    },
+    lock: function() {
+        var self = this;
+        if (self._changed) {
+            return;
         }
-    });
-}));
+        if (!self.options.url) {
+            return;
+        }
+        self._changed = true;
+        $.ajax({
+            url: self.options.url,
+            data: {
+                "lock": true
+            },
+            success: this.inject_response.bind(this)
+        });
+    },
+    unlock: function() {
+        var self = this;
+        if (!self._changed) {
+            return;
+        }
+        if (!self.options.url) {
+            return;
+        }
+        $.ajax({
+            url: self.options.url,
+            data: {
+                "unlock": true
+            },
+            success: this.inject_response.bind(this)
+        });
+        self._changed = false;
+    },
+    set_listeners: function() {
+        var self = this;
+        if (!self.$el.is("form")) {
+            return;
+        }
+        // unlock when changing page
+        $(window).on(
+            "beforeunload",
+            self.unlock.bind(self)
+        );
+
+        // unlock when the form gets removed from the DOM
+        self.$el.bind("DOMNodeRemoved", function(e) {
+            if (e.target === self.$el[0]) {
+                self.unlock.bind(self)();
+            }
+        });
+
+        // lock when elements are changed
+        $(self.defaults.changingFields, self.$el).on(
+            self.defaults.changingEvents,
+            self.lock.bind(self)
+        );
+    }
+});
